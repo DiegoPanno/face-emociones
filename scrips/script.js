bcc7cadb9dff0263e81c29d79068fe596f04b14a
Diego Panno <diegompanno@gmail.com>
2024-07-23 00:58:43 -0300

    ajustes

--- scrips/script.js
+++ scrips/script.js
@@ -22,13 +22,13 @@ const cargarCamara = () => {
 };
 
 Promise.all([
-  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"), 
-  faceapi.nets.ageGenderNet.loadFromUri("/models"),
-  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-  faceapi.nets.faceLandmark68TinyNet.loadFromUri("/models"),
-  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.ssdMobilenetv1.loadFromUri("./models"), 
+  faceapi.nets.ageGenderNet.loadFromUri("./models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("./models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+  faceapi.nets.faceLandmark68TinyNet.loadFromUri("./models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+  faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
 ]).then(() => {
   console.log("Modelos cargados");
   cargarCamara();
